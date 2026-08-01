bf798a3e0fc10e8b48a811837d68718aaa644d2e
nolight132 <contact@nolight.dev>
2026-08-01 23:50:32 +0200

    feat(programs): add bat and nls with ls/cat aliases

--- modules/programs/configured/fish.nix
+++ modules/programs/configured/fish.nix
@@ -1,12 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
+let
+  system = pkgs.stdenv.hostPlatform.system;
+in
 {
   programs.fish.enable = true;
+
+  environment.shellAliases = {
+    ls = "nls";
+    cat = "bat";
+  };
 
   environment.systemPackages = with pkgs; [
     starship
     zoxide
     atuin
     fzf
+    bat
+
+    inputs.nls.packages.${system}.default
   ];
 }

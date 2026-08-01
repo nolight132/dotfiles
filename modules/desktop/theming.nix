10427ea1db0d5e78ac9fba83e78002e3e449d06e
nolight132 <contact@nolight.dev>
2026-08-01 23:50:24 +0200

    feat(desktop): configure qt, cursor theme and dconf defaults

--- modules/desktop/theming.nix
+++ modules/desktop/theming.nix
@@ -1,6 +1,29 @@
 { pkgs, ... }:
 
 {
+  qt.enable = true;
+
+  environment.sessionVariables = {
+    XCURSOR_THEME = "Breeze_Light";
+    XCURSOR_SIZE = "24";
+  };
+
+  programs.dconf = {
+    enable = true;
+    profiles.user.databases = [
+      {
+        settings."org/gnome/desktop/interface" = {
+          icon-theme = "Papirus";
+          font-name = "Noto Sans 10";
+          document-font-name = "Noto Sans 10";
+          monospace-font-name = "JetBrains Mono 10";
+          font-hinting = "slight";
+          font-antialiasing = "grayscale";
+        };
+      }
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     # Themes
     adwaita-icon-theme

ecc0d5a445f139e1794b0e3dd62e639afd53d0e0
nolight132 <contact@nolight.dev>
2026-08-03 13:49:06 +0200

    fix(vicinae): keep launched apps alive across restarts

--- modules/services/vicinae.nix
+++ modules/services/vicinae.nix
@@ -19,8 +19,10 @@
 
     serviceConfig = {
       ExecStart = "${pkgs.vicinae}/bin/vicinae server";
+      ExecStopPost = "-${pkgs.procps}/bin/pkill --full --uid nolight %t/vicinae/extension-manager.js";
       Restart = "on-failure";
       RestartSec = 1;
+      KillMode = "process";
     };
   };
 }

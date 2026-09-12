d91a7164782ddc2879a8ec3fc41bc1d2e4b0493d
nolight132 <contact@nolight.dev>
2026-09-12 16:02:10 +0200

    feat(mime): open images in loupe and directories in dolphin

--- home/mime.nix
+++ home/mime.nix
@@ -1,5 +1,35 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  imageTypes = [
+    "image/apng"
+    "image/avif"
+    "image/bmp"
+    "image/gif"
+    "image/heic"
+    "image/jp2"
+    "image/jpeg"
+    "image/jxl"
+    "image/png"
+    "image/qoi"
+    "image/svg+xml"
+    "image/svg+xml-compressed"
+    "image/tiff"
+    "image/vnd.microsoft.icon"
+    "image/webp"
+    "image/x-dds"
+    "image/x-exr"
+    "image/x-portable-anymap"
+    "image/x-portable-bitmap"
+    "image/x-portable-graymap"
+    "image/x-portable-pixmap"
+    "image/x-qoi"
+    "image/x-tga"
+    "image/x-win-bitmap"
+    "image/x-xbitmap"
+    "image/x-xpixmap"
+  ];
+in
 {
   xdg.mimeApps = {
     enable = true;
@@ -9,7 +39,10 @@
       "x-scheme-handler/http" = [ "zen.desktop" ];
       "x-scheme-handler/https" = [ "zen.desktop" ];
 
+      "inode/directory" = [ "org.kde.dolphin.desktop" ];
+
       "application/pdf" = [ "org.gnome.Evince.desktop" ];
-    };
+    }
+    // lib.genAttrs imageTypes (_: [ "org.gnome.Loupe.desktop" ]);
   };
 }
